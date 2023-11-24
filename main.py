f8d7814184fdd9b68e9f7c6fca54692887aab15c
Pedro <jacques.pedro@gmail.com>
2023-11-23 20:56:03 -0600

    Commit in batches. Start from oldest

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ from googleapiclient.discovery import build
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 TOKEN_PICKLE_FILE = 'token.pickle'
 
+# Reuse a connection
+connection = sqlite3.connect('gmail_data.db')
+
 
 def get_gmail_service():
     # Load credentials from file if available, otherwise authenticate and save
@@ -50,28 +53,25 @@ def get_message(service, user_id='me', msg_id=''):
 
 # Function to initialize the database
 def initialize_database():
-    connection = sqlite3.connect('gmail_data.db')
     cursor = connection.cursor()
 
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS emails (
             message_id TEXT PRIMARY KEY,
-            date_received TEXT,
+            date_received BIGINT,
             from_email TEXT,
             domain_origin TEXT,
-            size_of_email TEXT,
+            size_of_email INTEGER,
             has_attachments INTEGER,
             subject TEXT
         )
     ''')
 
-    connection.commit()
-    connection.close()
+    commit_changes()
 
 
 # Function to insert an email into the database
 def insert_email(message_id, date_received, from_email, domain_origin, size_of_email, has_attachments, subject):
-    connection = sqlite3.connect('gmail_data.db')
     cursor = connection.cursor()
 
     cursor.execute('''
@@ -79,51 +79,69 @@ def insert_email(message_id, date_received, from_email, domain_origin, size_of_e
         VALUES (?, ?, ?, ?, ?, ?, ?)
     ''', (message_id, date_received, from_email, domain_origin, size_of_email, has_attachments, subject))
 
-    connection.commit()
-    connection.close()
 
-
-def list_messages(service, user_id='me', start_message_id=None, max_results=None):
+def list_messages(service, user_id='me', start_message_epoch=None, max_results=None):
     messages = []
     page_token = None
-    query = f'after:{int(get_message(service, msg_id=start_message_id)["internalDate"]) // 1000}' if start_message_id else None
+    query = f'after:{start_message_epoch // 1000}' if start_message_epoch else None
+
+    total_messages = get_profile(service)  # Get the total number of messages
+    messages_processed = 0
+    start_time = time.time()
 
     while True:
         response = service.users().messages().list(userId=user_id, q=query, maxResults=max_results,
                                                    pageToken=page_token).execute()
-        addtl_msgs = response.get('messages', [])
-        print(f'messages added to the queue: {addtl_msgs}')
-        messages.extend(addtl_msgs)
-        print(f'total messages in queue: {len(messages)}')
+        messages.extend(response.get('messages', []))
+
+        # Increment messages_processed based on the number of messages in the current response
+        messages_processed += len(response.get('messages', []))
 
         # Check if there are more messages
         if 'nextPageToken' in response:
             page_token = response['nextPageToken']
+
+            # Calculate estimated time to complete based on progress
+            elapsed_time = time.time() - start_time
+            estimated_time_to_complete = (total_messages - messages_processed) * (
+                    elapsed_time / messages_processed) if messages_processed > 0 else 0
+
+            # Print remaining estimated time
+            print(f"STAGE 1 of 2: Remaining Estimated Time: {estimated_time_to_complete:.2f} seconds")
 
             # Add a pause to avoid exceeding the rate limit
             time.sleep(0.5)  # You can adjust the duration based on your needs
         else:
             break
 
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+
+    # Calculate average time taken per message
+    average_time_per_message = elapsed_time / messages_processed if messages_processed > 0 else 0
+
+    # Print final statistics
+    print(f"STAGE 1 Total Messages Queued: {messages_processed}")
+    print(f"Average Time Per Message: {average_time_per_message:.2f} seconds")
+
+    messages.reverse()
     return messages
 
 
-# Function to get the last processed message ID from the database
+# Function to get the last processed message epoch from the database
 def get_last_processed_message():
-    connection = sqlite3.connect('gmail_data.db')
     cursor = connection.cursor()
 
-    cursor.execute('SELECT MAX(message_id) FROM emails')
-    last_processed_message_id = cursor.fetchone()[0]
+    cursor.execute('SELECT MAX(date_received) FROM emails')
+    last_processed_message_epoch = cursor.fetchone()[0]
 
-    connection.close()
-
-    return last_processed_message_id
+    return last_processed_message_epoch
 
 
 def get_subject(message):
-    subject_header = next((header['value'] for header in message['payload']['headers'] if header['name'] == 'Subject'), None)
-    return subject_header[:20] if subject_header else None
+    subject_header = next((header['value'] for header in message['payload']['headers'] if header['name'] == 'Subject'),
+                          None)
+    return subject_header[:30] if subject_header else None
 
 
 def get_from_email(message):
@@ -138,6 +156,14 @@ def get_from_email(message):
     else:
         return from_header
 
+
+def commit_changes():
+    connection.commit()
+    # Optionally: Close and reopen the connection if needed
+    # connection.close()
+    # connection = sqlite3.connect('gmail_data.db')
+
+
 # Main function
 def main():
     service = get_gmail_service()
@@ -145,39 +171,70 @@ def main():
     # Initialize the database
     initialize_database()
 
-    # Get the number of total messages
-    total_messages = get_profile(service)
-    print(f"Total messages in the account: {total_messages}")
-
     # Get the ID of the last processed message
-    last_processed_message_id = get_last_processed_message()
-    print(f"Last processed message ID: {last_processed_message_id}")
+    last_processed_message_epoch = get_last_processed_message()
+    last_processed_message_epoch = 1690470225000
+    print(f"Last processed message ID: {last_processed_message_epoch}")
 
     # List and process messages starting from the last processed message
-    messages = list_messages(service, start_message_id=last_processed_message_id)
+    messages = list_messages(service, start_message_epoch=last_processed_message_epoch)
+    messages_to_insert = len(messages)
+    print(f"Total Newer messages: {messages_to_insert}")
+    batches_processed = 0
+    start_time = time.time()
 
-    for msg in messages:
+    batch_size = 500
+
+    for i, msg in enumerate(messages, 1):
         message = get_message(service, msg_id=msg['id'])
-        date_received = message['internalDate']
+        date_received = int(message['internalDate'])
         from_email = get_from_email(message)
         domain_origin = from_email.split('@')[1] if '@' in from_email else ''
-        size_of_email = message['sizeEstimate']
+        size_of_email = int(message['sizeEstimate'])
         has_attachments = 1 if 'parts' in message['payload'] else 0
         subject = get_subject(message)
 
         # Print information of the message
-        print(f"Message ID: {msg['id']}")
         print(f"Date Received: {date_received}")
-        print(f"From Email: {from_email}")
-        print(f"Domain Origin: {domain_origin}")
-        print(f"Size of Email: {size_of_email} bytes")
-        print(f"Has Attachments: {'Yes' if has_attachments else 'No'}")
-        print(f"Subject: {subject}")
-        print("-" * 50)
 
         # Insert information into the database
         insert_email(msg['id'], date_received, from_email, domain_origin, size_of_email, has_attachments, subject)
 
+        # Commit changes in batches
+        if i % batch_size == 0:
+            commit_changes()
+
+            # Calculate estimated time to complete based on progress
+            batches_processed = + 1
+            elapsed_time = time.time() - start_time
+            estimated_time_to_complete = (messages_to_insert - (batches_processed * batch_size)) * (
+                    elapsed_time / (batches_processed * batch_size)) if batches_processed > 0 else 0
+
+            # Print remaining estimated time
+            print(f"STAGE 2 of 2: Remaining Estimated Time: {estimated_time_to_complete:.2f} seconds")
+
+    # Commit any remaining changes
+    commit_changes()
+    batches_processed = + 1
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+
+    # Calculate average time taken per message
+    average_time_per_message = elapsed_time / batches_processed if batches_processed > 0 else 0
+
+    # Print final statistics
+    print(f"STAGE 2 Total Batches Inserted: {batches_processed}")
+    print(f"Average Time Per Batch Insert: {average_time_per_message:.2f} seconds")
+
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    finally:
+        # Close the connection in the finally block to ensure it's always closed
+        connection.close()
